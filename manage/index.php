c87d6f4bda6b3926b079bbda7c919b7868aec656
grumpy-anonsaba <nathanaeley@gmail.com>
2020-05-02 14:47:49 -0500

    Added index.php to manage folder

---
+++ manage/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// Anonsaba 3.0 Management console
+
+// First lets grab all the files we need!
+require_once realpath(dirname(__DIR__)).'/config/config.php';
+require_once svrpath.'modules/management/manage.php';
+
