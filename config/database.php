b89810b1479cb36c4865ece084e036410b2204e2
grumpy-anonsaba <nathanaeley@gmail.com>
2020-05-01 21:45:07 -0500

    Added GetOne/GetAll to database.php

--- config/database.php
+++ config/database.php
@@ -9,9 +9,36 @@ class Database extends PDO {
 			try {
 				$stmt = $this->prepare($query);
 				$stmt->execute();
+				$stmt->closeCursor();
+				unset($stmt);
 			}
 			catch (PDOException $e) {
 				echo $e->getMessage();
 			}
 		}
+		public function GetOne($query) {
+			try {
+				$stmt = $this->prepare($query);
+				$stmt->execute();
+				$result = $stmt->fetch(PDO::FETCH_ASSOC);
+				$stmt->closeCursor();
+				return (is_array($result)) ? array_shift($result) : $result;
+				unset($stmt);
+			}
+			catch (PDOException $e) {
+				echo $e->getMessage();
+			}
+		}
+		public function GetAll($query) {
+			try {
+				$stmt = $this->prepare($query);
+				$stmt->execute();
+				$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+				$stmt->closeCursor();
+				unset($stmt);
+			}
+			catch (PDOException $e) {
+				echo $e->getMessage();
+			}
+		}			
 }
