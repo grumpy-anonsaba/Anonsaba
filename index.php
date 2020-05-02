63baad4b6f226fb814eb0aa8cf9fd86a72698a91
grumpy-anonsaba <nathanaeley@gmail.com>
2020-05-01 20:45:58 -0500

    changed fullpath -> svrpath

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 	require './config/config.php';
 	require './modules/core.php';
 	//Is Anonsaba even installed?
-	if (!file_exists(fullpath.'.installed')) {
+	if (!file_exists(svrpath.'.installed')) {
 		Core::Error('It appears you haven\'t installed Anonsaba 3.0 please click <a href="/INSTALL/install.php">here</a> to install!');
 	}
