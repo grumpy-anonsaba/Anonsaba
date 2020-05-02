3ce4357652430e9288867f363cb21c04aa753952
grumpy-anonsaba <nathanaeley@gmail.com>
2020-05-02 13:06:58 -0500

    Got rid of the if statements and swapped them to cases. This makes life easier for anyone who just wants to add stuff to the front page

--- index.php
+++ index.php
@@ -6,17 +6,24 @@
 	if (!file_exists(svrpath.'.installed')) {
 		Core::Error('It appears you haven\'t installed Anonsaba 3.0 please click <a href="/INSTALL/install.php">here</a> to install!');
 	}
+	// Great lets configure our values then output them!
 	$twig_data['sitename'] = Core::GetConfigOption('sitename');
 	$twig_data['slogan'] = Core::GetConfigOption('slogan');
 	$twig_data['version'] = Core::GetConfigOption('version');
 	$twig_data['irc'] = Core::GetConfigOption('irc');
-	isset($_GET['view']) ? $_GET['view'] : '';
-	if ($_GET['view'] == '') {
-		$entires = $db->GetAll('SELECT * FROM '.dbprefix.'front WHERE type = '.$db->quote('news').' ORDER BY date DESC LIMIT 5 OFFSET '.($_GET['page'] * 5));
-	} elseif ($_GET['view'] == 'faq') {
-		$entires = $db->GetAll('SELECT * FROM '.dbprefix.'front WHERE type = '.$db->quote('faq').' ORDER BY date DESC');
-	} elseif ($_GET['view'] == 'rules') {
-		$entries = $db->GetAll('SELECT FROM '.dbprefix.'front WHERE type = '.$db->quote('rules').' ORDER BY date ASC');
+	switch($_GET['view']) {
+		case '': 
+			$entires = $db->GetAll('SELECT * FROM '.dbprefix.'front WHERE type = '.$db->quote('news').' ORDER BY date DESC LIMIT 5 OFFSET '.($_GET['page'] * 5));
+			break;
+		case 'faq':
+			$entires = $db->GetAll('SELECT * FROM '.dbprefix.'front WHERE type = '.$db->quote('faq').' ORDER BY date DESC');
+			break;
+		case 'rules':
+			$entries = $db->GetAll('SELECT FROM '.dbprefix.'front WHERE type = '.$db->quote('rules').' ORDER BY date ASC');
+			break;
+		default:
+			$entires = $db->GetAll('SELECT * FROM '.dbprefix.'front WHERE type = '.$db->quote('news').' ORDER BY date DESC LIMIT 5 OFFSET '.($_GET['page'] * 5));
+			break;
 	}
 	$twig_data['entries'] = $entries;
 	$twig_data['view'] = $_GET['view'];
